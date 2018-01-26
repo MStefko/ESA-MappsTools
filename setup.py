1a78d39de7cf60003eb07b4bad1d85ad91f11d73
Marcel Stefko <marcel.stefko@esa.int>
2018-01-26 16:32:53 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SpiceTools',
+    version='0.1',
+    packages=['', 'tests', 'mosaics'],
+    url='https://gitlab.esa.int/MarcelStefko/SPICE_tools/',
+    license='Proprietarycode ',
+    author='Marcel Stefko',
+    author_email='marcel.stefko@esa.int',
+    description='',
+    test_suite='tests'
+)
