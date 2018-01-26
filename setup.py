f79ff29600eeb52a0984db629d6f17669a2a2c93
Marcel Stefko <marcel.stefko@esa.int>
2018-01-26 17:07:05 +0100

    Include non-python files in tests/ folder in setup

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='SpiceTools',
     version='0.1',
-    packages=['', 'tests', 'mosaics'],
     url='https://gitlab.esa.int/MarcelStefko/SPICE_tools/',
     license='Proprietarycode ',
     author='Marcel Stefko',
     author_email='marcel.stefko@esa.int',
     description='',
     test_suite='tests',
-    install_requires=['numpy','six','matplotlib','iso8601','spiceypy','shapely','pandas']
+    install_requires=['numpy','six','matplotlib','iso8601','spiceypy','shapely','pandas'],
+    packages = find_packages(),
+    package_data = {'': ['tests/*']},
+    include_package_data = True
 )
