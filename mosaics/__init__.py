63abb888d6ecbeb4fcceff93df8bd951e057c9a0
Marcel Stefko <marcel.stefko@esa.int>
2018-02-21 15:20:00 +0100

    Add docstring, expose JanusMosaicGenerator and MajisScanGenerator

--- mosaics/__init__.py
+++ mosaics/__init__.py
@@ -1,2 +1,10 @@
 # coding=utf-8
+""" Mosaics module: This module provides two instrument-specific generators:
+
+- JanusMosaicGenerator: Creating mosaics optimized for JUICE-JANUS imaging.
+- MajisScanGenerator: Creating scans (series of vertical slews) optimized for
+  JUICE-MAJIS imaging.
+"""
+
 from .JanusMosaicGenerator import JanusMosaicGenerator
+from .MajisScanGenerator import MajisScanGenerator
