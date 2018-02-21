6c824f9385a58c1200e5af35244de069b4f72e6f
Marcel Stefko <marcel.stefko@esa.int>
2018-02-21 15:20:39 +0100

    Rename DiskMosaicGenerator to MosaicGenerator

--- tests/test_mosaicGenerator.py
+++ tests/test_mosaicGenerator.py
@@ -1,18 +1,18 @@
 from unittest import TestCase
 from unittest.mock import patch, MagicMock
-from mosaics.DiskMosaicGenerator import DiskMosaicGenerator
+from mosaics.MosaicGenerator import MosaicGenerator
 
 from datetime import datetime
 
 valid_start_time = datetime.strptime("2031-04-26T00:40:47", "%Y-%m-%dT%H:%M:%S")
 
-class TestDiskMosaicGenerator(TestCase):
+class TestMosaicGenerator(TestCase):
 
     @patch('mosaics.DiskMosaic.spy.str2et', return_value=1.0)
-    @patch('mosaics.DiskMosaicGenerator.get_body_angular_diameter_rad', return_value=0.19167923151263316)
+    @patch('mosaics.MosaicGenerator.get_body_angular_diameter_rad', return_value=0.19167923151263316)
     def test_generate_symmetric_mosaic(self, mock_diam, mock_str2et):
-        dmg = DiskMosaicGenerator((3.0, 2.0), "JUICE", "CALLISTO", valid_start_time, "min",
-                      "deg", 2.0, 0.04*60)
+        dmg = MosaicGenerator((3.0, 2.0), "JUICE", "CALLISTO", valid_start_time, "min",
+                      "deg", 2.0, 0.04 * 60)
         dm = dmg.generate_symmetric_mosaic(margin=0.1)
         center_points = [(-4.54032604229169, -5.04032604229169), (-4.54032604229169, -3.360217361527793),
                          (-4.54032604229169, -1.6801086807638965), (-4.54032604229169, 0.0),
@@ -33,7 +33,7 @@ class TestDiskMosaicGenerator(TestCase):
         self.assertAlmostEqual(dm.center_points, center_points, places=4)
 
     def test__optimize_steps_centered(self):
-        osc = DiskMosaicGenerator._optimize_steps_centered
+        osc = MosaicGenerator._optimize_steps_centered
 
         with self.assertRaises(ValueError, msg="Overlap between 0.0 (incl.) and 1.0 (excl.)"):
             osc(5.0, 2.0, -0.3)
@@ -51,9 +51,9 @@ class TestDiskMosaicGenerator(TestCase):
 
         self.assertEqual(osc(10, 1, 0.9), (91, -4.5, 0.1))
 
-    @patch('mosaics.DiskMosaicGenerator.DiskMosaic')
+    @patch('mosaics.MosaicGenerator.DiskMosaic')
     def test_generate_symmetric_mosaic_explicit_values(self, mock_DiskMosaic):
-        fun = DiskMosaicGenerator.generate_symmetric_mosaic
+        fun = MosaicGenerator.generate_symmetric_mosaic
         dmg = MagicMock()
         dmg.target_angular_diameter = 2.5
         dmg._optimize_steps_centered.side_effect = 10*[(2, -3.6, 1.2), (4, -2.3, -1.8)]
